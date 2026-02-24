ef316554725b87033860aee5fcf71a0a5e542089
“JingleH” <jingleh321@gmail.com>
2026-02-24 16:41:38 -0600

    handle lint

--- dc-shared/blocks/verb-widget/verb-widget.js
+++ dc-shared/blocks/verb-widget/verb-widget.js
@@ -315,6 +315,7 @@ function addCookieIfSubdomain() {
   }
 }
 
+// eslint-disable-next-line no-unused-vars
 function initPrerender(url) {
   if (!url || !HTMLScriptElement?.supports('speculationrules') || !isSubdomain()) {
     return;
@@ -338,8 +339,9 @@ function redDirLink(verb) {
   return newLocation;
 }
 
+// eslint-disable-next-line no-unused-vars
 function redDir(verb) {
-  //window.location.href = redDirLink(verb);
+  // window.location.href = redDirLink(verb);
 }
 
 function getSplunkEndpoint() {
