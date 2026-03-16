4840eed030e8802ee2dbc065e3f091be3ddad21d
Jingle Huang <32369333+JingleH@users.noreply.github.com>
2026-03-16 15:01:00 -0500

    Load libs from milo.adobe.com (#27)
    * load libs from milo.adobe.com
    
    * default prodLibs

--- dc-shared/blocks/unity/unity.js
+++ dc-shared/blocks/unity/unity.js
@@ -98,8 +98,8 @@ export const localeMap = {
 
 function getUnityLibs(prodLibs = '/unitylibs') {
   const { hostname, search } = window.location;
-  if (hostname === 'acrobat.adobe.com') return 'https://www.adobe.com/unitylibs';
-  if (hostname === 'stage.acrobat.adobe.com' && !(new URLSearchParams(search).get('unitylibs'))) return 'https://www.stage.adobe.com/unitylibs';
+  if (hostname === 'acrobat.adobe.com') return 'https://milo.adobe.com/unitylibs';
+  if (hostname === 'stage.acrobat.adobe.com' && !(new URLSearchParams(search).get('unitylibs'))) return 'https://milo.stage.adobe.com/unitylibs';
   if (!['.aem.', '.hlx.', '.stage.', 'local', '.da.'].some((i) => hostname.includes(i))) return prodLibs;
   // eslint-disable-next-line compat/compat
   const branch = new URLSearchParams(search).get('unitylibs') || 'main';
